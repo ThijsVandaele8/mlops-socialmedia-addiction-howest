a059b4dd83f80f9d08f08fb68cd85494eabb4abc
thijs <thijs.vandaele8@gmail.com>
2025-08-15 15:58:43 +0200

    [Actions workflow] Add create components step

---
+++ scripts/azure/create_components.ps1
@@ -0,0 +1,8 @@
+$components = Get-ChildItem -Path ".\aml\components" -File -Recurse |
+              Select-Object -ExpandProperty FullName
+
+foreach($component in $components)
+{
+    Write-Host "Create component $component"
+    az ml component create --file $environment.FullName
+}
