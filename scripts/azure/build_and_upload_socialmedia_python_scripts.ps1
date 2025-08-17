ff7e77ed38516115aa94c549524a26e668a31b33
thijs <thijs.vandaele8@gmail.com>
2025-08-17 14:08:58 +0200

    [Action workflow] Add ensure data exits step for source code

--- scripts/azure/build_and_upload_socialmedia_python_scripts.ps1
+++ scripts/azure/build_and_upload_socialmedia_python_scripts.ps1
@@ -1,14 +1,46 @@
 param (
     [Parameter(Mandatory=$true)]
+    [string]$contentPath,
+    [Parameter(Mandatory=$true)]
+    [string]$dataYamlPath,
+    [Parameter(Mandatory=$true)]
+    [string]$dataName,
+    [Parameter(Mandatory=$true)]
     [string]$workspaceName,
     [Parameter(Mandatory=$true)]
     [string]$resourceGroup
 )
-
+# Build
 python setup.py bdist_wheel
 
-$path = az ml data create --file './aml/data/code/socialmedia_sourcecode_wheel.yaml' --query "path" --output tsv
-$blobName = $path -replace ".*?/paths/", ""
+# Compare
+. "$PSScriptRoot\..\utils.ps1"
+
+$fullContentPath = (Resolve-Path $contentPath).Path
+Write-Host "Full content path: $fullContentPath"
+
+$dataInfo = Test-CloudFileHash -contentPath $fullContentPath -dataName $dataName
+
+if (-not $dataInfo.IsSame)
+{
+    $dataYamlContent = Get-Content $dataYamlPath
+    $dataYamlContent = $dataYamlContent -replace "{{hash}}", $dataInfo.LocalHash -replace "{{path}}", $fullContentPath
+
+    $tempYamlPath = "$env:TEMP\$dataName.yaml"
+    $dataYamlContent | Set-Content -Path $tempYamlPath -Encoding UTF8
+
+    $blobPath = az ml data create --file $tempYamlPath --query "path" --output tsv
+}
+else 
+{
+    $blobPath = az ml data show --name $dataName --version $dataInfo.CloudVersion --workspace-name $workspaceName --resource-group $resourceGroup --query "path" --output tsv
+}
+
+$displayVersion = if ($dataInfo.IsSame) { $dataInfo.CloudVersion } else { $version }
+Write-Host "Latest version: $displayVersion"
+
+# Generate Sas
+$blobName = $blobPath -replace ".*?/paths/", ""
 
 $datastoreInfo = az ml datastore show `
     --name workspaceblobstore `
